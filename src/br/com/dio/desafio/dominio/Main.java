192756c087525a9b852210643bc0895101abe7c4
YumiSuzana <suzanamuta@gmail.com>
2025-08-29 14:49:38 -0300

    arquivos criados para merge

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -1,5 +1,11 @@
 package br.com.dio.desafio.dominio;
 
+import br.com.dio.desafio.dominio.Conteudo;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.totalXP;
 
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
@@ -36,49 +42,74 @@ public class Main {
         bootcamp.setNome("GFT Start #7 -Java");
         bootcamp.setDescricao("Bootcamp completo para iniciantes");
 
+
 //        Adicionar os cursos e a mentoria ao único bootcamp:
         bootcamp.getConteudos().add(curso1);
         bootcamp.getConteudos().add(curso2);
         bootcamp.getConteudos().add(curso3);
         bootcamp.getConteudos().add(mentoria);
 
-//        Inserção da Dev
-        Dev dev = new Dev();
-        dev.setNome("YumiSuzana");
-        dev.inscreverBootcamp(bootcamp);
+//        Inserção de 2 Devs (pra fazer ranking)
+        Dev dev1 = new Dev();
+        dev1.setNome("Dev 1");
+        dev1.inscreverBootcamp(bootcamp);
 
-//        cursos concluidoss da YumiSuzana
-        Set<Conteudo> concluidosYS = new LinkedHashSet<>();
-        concluidosYS.add(curso1);
-        concluidosYS.add(curso2);
-        dev.setConteudosConcluidos(concluidosYS);
+        Dev dev2 = new Dev();
+        dev2.setNome("Dev 2");
+        dev2.inscreverBootcamp(bootcamp);
 
-//        cursos inscritos & mentoria da YumiSuzana:
-        Set <Conteudo> inscritosYS = new LinkedHashSet<>();
-        inscritosYS.add(curso1);
-        inscritosYS.add(curso2);
-        inscritosYS.add(curso3);
-        inscritosYS.add(mentoria);
-        dev.setConteudosInscritos(inscritosYS);
 
-//        começar a imprimir as infos.....
-        System.out.println("Dev: " + dev.getNome() + "\n \nCursos Inscritos: ");
 
-        dev.getConteudosInscritos().forEach(c ->
-                System.out.println("- " + c.getTitulo())
-        );
+//        cursos concluidoss
+        Set<Conteudo> concluidosY = new LinkedHashSet<>();
+        concluidosY.add(curso1);
+        concluidosY.add(curso2);
+        dev1.setConteudosConcluidos(concluidosY);
 
-        System.out.println("\nCursos ja concluidos: ");
+        Set<Conteudo> concluidosS = new LinkedHashSet<>();
+        concluidosS.add(curso1);
+        concluidosS.add(mentoria);
+        dev2.setConteudosConcluidos(concluidosS);
 
-        AtomicReference<Double> total = new AtomicReference<>(10d);
 
-        dev.getConteudosConcluidos().forEach(c -> {
-            System.out.println("- " + c.getTitulo());
-            total.updateAndGet(v -> new Double((double) (v + c.calcularXp())));
+//        cursos inscritos & mentoria:
+        Set <Conteudo> inscritosY = new LinkedHashSet<>();
+        inscritosY.add(curso1);
+        inscritosY.add(curso2);
+        inscritosY.add(curso3);
+        inscritosY.add(mentoria);
+        dev1.setConteudosInscritos(inscritosY);
 
-        });
+        Set <Conteudo> inscritosS = new LinkedHashSet<>();
+        inscritosS.add(curso1);
+        inscritosS.add(curso2);
+        inscritosS.add(curso3);
+        inscritosS.add(mentoria);
+        dev2.setConteudosInscritos(inscritosS);
 
-        System.out.println("\nTotal de XP até o momento: " + total + "\nContinue assim, " + dev.getNome() + "!");
+
+
+
+
+        totalXP totalDev1 = new totalXP();
+        totalXP totalDev2 = new totalXP();
+
+        if (totalDev1.xpIndiv(dev1) > totalDev2.xpIndiv(dev2)) {
+
+            System.out.println("\nParabéns, " + dev1.getNome() + " você está com mais pontos!");
+
+        } else if (totalDev1.xpIndiv(dev1) < totalDev2.xpIndiv(dev2)){
+
+            System.out.println("\nParabéns, " + dev2.getNome() + " você está com mais pontos!");
+
+        } else {
+
+            System.out.println("\nA competição está intensa! Parabéns, " + dev2.getNome() +" e " + dev2.getNome());
+
+
+        }
+
+
 
 
     }
--- src/br/com/dio/desafio/dominio/totalXP.java
+++ src/br/com/dio/desafio/dominio/totalXP.java
@@ -1,4 +1,29 @@
 package br.com.dio.desafio.dominio;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class totalXP {
+
+    public static double xpIndiv(Dev dev) {
+        System.out.println("Dev: " + dev.getNome() + "\n \nConteudos Inscritos: ");
+
+        dev.getConteudosInscritos().forEach(c ->
+                System.out.println("- " + c.getTitulo())
+        );
+
+        System.out.println("\nConteudos ja concluidos: ");
+
+        AtomicReference<Double> total = new AtomicReference<>(10d);
+
+        dev.getConteudosConcluidos().forEach(c -> {
+            System.out.println("- " + c.getTitulo());
+            total.updateAndGet(v -> new Double((double) (v + c.calcularXp())));
+
+        });
+
+        System.out.println("XP total de " + dev.getNome() + ": " + total);
+
+        return total.get();
+
+    }
 }
